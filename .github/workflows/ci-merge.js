2740e9054c7328f3320aefe752134632e2f1c494
RyzenDew <84041391+RyzenDew@users.noreply.github.com>
2024-03-16 12:14:30 -0300

    Update ci-merge.js

--- .github/workflows/ci-merge.js
+++ .github/workflows/ci-merge.js
@@ -200,7 +200,7 @@ async function mergebot(github, context, execa) {
     }
     console.info("The following pull requests will be merged:");
     console.table(displayList);
-    await fetchPullRequests(pulls, "https://github.com/citra-emu/citra", execa);
+    await fetchPullRequests(pulls, "https://github.com/RyzenDew/Lime-3DS-Emulator", execa);
     const mergeResults = await mergePullRequests(pulls, execa);
     await generateReadme(pulls, context, mergeResults, execa);
     await tagAndPush(github, context.repo.owner, `${context.repo.repo}-canary`, execa, true);
