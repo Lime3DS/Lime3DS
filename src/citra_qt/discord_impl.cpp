2639fec40a6d0631e668d52ff512deba06eb809e
Riley Hawksworth <rileyrooh13@gmail.com>
2024-03-26 17:21:41 +0000

    Update Discord RPC to use 'Lime' instead of 'Citra'.

--- src/citra_qt/discord_impl.cpp
+++ src/citra_qt/discord_impl.cpp
@@ -18,7 +18,7 @@ DiscordImpl::DiscordImpl(const Core::System& system_) : system{system_} {
 
     // The number is the client ID for Citra, it's used for images and the
     // application name
-    Discord_Initialize("719647875465871400", &handlers, 1, nullptr);
+    Discord_Initialize("1222229231367487539", &handlers, 1, nullptr);
 }
 
 DiscordImpl::~DiscordImpl() {
@@ -41,8 +41,8 @@ void DiscordImpl::Update() {
     }
 
     DiscordRichPresence presence{};
-    presence.largeImageKey = "citra";
-    presence.largeImageText = "Citra is an emulator for the Nintendo 3DS";
+    presence.largeImageKey = "large_icon";
+    presence.largeImageText = "Lime is an emulator for the 3DS!";
     if (is_powered_on) {
         presence.state = title.c_str();
         presence.details = "Currently in game";
