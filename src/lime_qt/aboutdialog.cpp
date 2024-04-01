3906ac754c2b88acb9fa500e6e80d184640b58ff
Riley Hawksworth <rileyrooh13@gmail.com>
2024-04-01 23:04:01 +0100

    Update the about UI in lime_qt.

--- src/lime_qt/aboutdialog.cpp
+++ src/lime_qt/aboutdialog.cpp
@@ -11,7 +11,7 @@ AboutDialog::AboutDialog(QWidget* parent)
     : QDialog(parent, Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowSystemMenuHint),
       ui(std::make_unique<Ui::AboutDialog>()) {
     ui->setupUi(this);
-    ui->labelLogo->setPixmap(QIcon::fromTheme(QStringLiteral("citra")).pixmap(200));
+    ui->labelLogo->setPixmap(QIcon::fromTheme(QStringLiteral("lime")).pixmap(200));
     ui->labelBuildInfo->setText(ui->labelBuildInfo->text().arg(
         QString::fromUtf8(Common::g_build_fullname), QString::fromUtf8(Common::g_scm_branch),
         QString::fromUtf8(Common::g_scm_desc), QString::fromUtf8(Common::g_build_date).left(10)));
