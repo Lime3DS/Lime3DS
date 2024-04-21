1b4e8ff1bdf0f430a1671fb1b5a8b70911cf431e
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2024-04-21 13:16:26 +0200

    Update EmulationFragment.kt

--- src/android/app/src/main/java/io/github/lime3ds/android/fragments/EmulationFragment.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/fragments/EmulationFragment.kt
@@ -422,7 +422,7 @@ class EmulationFragment : Fragment(), SurfaceHolder.Callback, Choreographer.Fram
     private fun rotateScreen() {
         if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
             (context as? EmulationActivity)?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        } else if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSPACE) {
+        } else if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             (context as? EmulationActivity)?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         } else {
             (context as? EmulationActivity)?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
