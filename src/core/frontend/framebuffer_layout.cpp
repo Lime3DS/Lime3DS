743a72ff75d37893e4f965df26a0190b8a69cb4d
OpenSauce04 <opensauce04@gmail.com>
2024-05-26 21:10:57 +0100

    Custom Layout options now uses X, Y, Width Height
    Previously used Left, Top, Right, Bottom positions

--- src/core/frontend/framebuffer_layout.cpp
+++ src/core/frontend/framebuffer_layout.cpp
@@ -379,12 +379,16 @@ FramebufferLayout CustomFrameLayout(u32 width, u32 height, bool is_swapped) {
 
     Common::Rectangle<u32> top_screen{Settings::values.custom_top_left.GetValue(),
                                       Settings::values.custom_top_top.GetValue(),
-                                      Settings::values.custom_top_right.GetValue(),
-                                      Settings::values.custom_top_bottom.GetValue()};
+                                      (u32)(Settings::values.custom_top_left.GetValue() +
+                                            Settings::values.custom_top_right.GetValue()),
+                                      (u32)(Settings::values.custom_top_top.GetValue() +
+                                            Settings::values.custom_top_bottom.GetValue())};
     Common::Rectangle<u32> bot_screen{Settings::values.custom_bottom_left.GetValue(),
                                       Settings::values.custom_bottom_top.GetValue(),
-                                      Settings::values.custom_bottom_right.GetValue(),
-                                      Settings::values.custom_bottom_bottom.GetValue()};
+                                      (u32)(Settings::values.custom_bottom_left.GetValue() +
+                                            Settings::values.custom_bottom_right.GetValue()),
+                                      (u32)(Settings::values.custom_bottom_top.GetValue() +
+                                            Settings::values.custom_bottom_bottom.GetValue())};
 
     if (is_swapped) {
         res.top_screen = bot_screen;
