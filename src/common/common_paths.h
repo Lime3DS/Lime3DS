d2a5260d625f105a5ae10e67df0c8c2baad84667
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-06-08 22:26:52 -0600

    common_paths.h: Migrate user directory from Citra to Lime3DS

--- src/common/common_paths.h
+++ src/common/common_paths.h
@@ -18,18 +18,18 @@
 #define EMU_DATA_DIR USER_DIR
 #else
 #ifdef _WIN32
-#define EMU_DATA_DIR "Citra"
+#define EMU_DATA_DIR "Lime3DS"
 #elif defined(__APPLE__)
 #include <TargetConditionals.h>
 #if TARGET_OS_IPHONE
-#define APPLE_EMU_DATA_DIR "Documents" DIR_SEP "Citra"
+#define APPLE_EMU_DATA_DIR "Documents" DIR_SEP "Lime3DS"
 #else
-#define APPLE_EMU_DATA_DIR "Library" DIR_SEP "Application Support" DIR_SEP "Citra"
+#define APPLE_EMU_DATA_DIR "Library" DIR_SEP "Application Support" DIR_SEP "Lime3DS"
 #endif
 // For compatibility with XDG paths.
-#define EMU_DATA_DIR "citra-emu"
+#define EMU_DATA_DIR "lime3ds-emu"
 #else
-#define EMU_DATA_DIR "citra-emu"
+#define EMU_DATA_DIR "lime3ds-emu"
 #endif
 #endif
 
