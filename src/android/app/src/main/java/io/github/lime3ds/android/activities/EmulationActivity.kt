5c791f25969b6038417aba1ea0f1430eb50cb7b5
Felix Nüsse <felix.nuesse@t-online.de>
2024-07-04 23:55:43 +0200

    add missing import

--- src/android/app/src/main/java/io/github/lime3ds/android/activities/EmulationActivity.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/activities/EmulationActivity.kt
@@ -9,6 +9,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
