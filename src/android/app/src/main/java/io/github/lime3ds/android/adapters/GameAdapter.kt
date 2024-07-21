a9bacfda40546ec4d162fb529f083e39db138a8c
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-07-21 13:27:57 +0200

    android: Implement about game dialog

--- src/android/app/src/main/java/io/github/lime3ds/android/adapters/GameAdapter.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/adapters/GameAdapter.kt
@@ -10,6 +10,8 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Context
+import android.widget.TextView
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +25,8 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
 import io.github.lime3ds.android.HomeNavigationDirections
 import io.github.lime3ds.android.LimeApplication
 import io.github.lime3ds.android.R
@@ -32,8 +36,11 @@ import io.github.lime3ds.android.features.cheats.ui.CheatsFragmentDirections
 import io.github.lime3ds.android.model.Game
 import io.github.lime3ds.android.utils.GameIconUtils
 import io.github.lime3ds.android.viewmodel.GamesViewModel
+import io.github.lime3ds.android.features.settings.ui.SettingsActivity
+import io.github.lime3ds.android.features.settings.utils.SettingsFile
 
-class GameAdapter(private val activity: AppCompatActivity) :
+
+class GameAdapter(private val activity: AppCompatActivity, private val inflater: LayoutInflater) :
     ListAdapter<Game, GameViewHolder>(AsyncDifferConfig.Builder(DiffCallback()).build()),
     View.OnClickListener, View.OnLongClickListener {
     private var lastClickTime = 0L
@@ -83,7 +90,7 @@ class GameAdapter(private val activity: AppCompatActivity) :
     }
 
     /**
-     * Opens the cheats settings for the game that was clicked on.
+     * Opens the GameAboutDialog for the game that was clicked on.
      *
      * @param view The view representing the game the user wants to play.
      */
@@ -99,8 +106,7 @@ class GameAdapter(private val activity: AppCompatActivity) :
                 .setPositiveButton(android.R.string.ok, null)
                 .show()
         } else {
-            val action = CheatsFragmentDirections.actionGlobalCheatsFragment(holder.game.titleId)
-            view.findNavController().navigate(action)
+            showGameAboutDialog(context, holder.game, holder, view)
         }
         return true
     }
@@ -187,6 +193,38 @@ class GameAdapter(private val activity: AppCompatActivity) :
                 3000
             )
         }
+    }
+
+    private fun showGameAboutDialog(context: Context, game: Game, holder: GameViewHolder, view: View) {
+        val bottomSheetView = inflater.inflate(R.layout.game_about_dialog, null)
+        
+        val bottomSheetDialog = BottomSheetDialog(context)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        bottomSheetView.findViewById<TextView>(R.id.game_title).text = game.title
+        bottomSheetView.findViewById<TextView>(R.id.game_author).text = game.company
+        GameIconUtils.loadGameIcon(activity, game, bottomSheetView.findViewById(R.id.game_icon))
+
+        bottomSheetView.findViewById<MaterialButton>(R.id.game_play).setOnClickListener {
+            val action = HomeNavigationDirections.actionGlobalEmulationActivity(holder.game)
+            view.findNavController().navigate(action)
+        }
+
+        bottomSheetView.findViewById<MaterialButton>(R.id.game_settings).setOnClickListener {
+            SettingsActivity.launch(
+                        context,
+                        SettingsFile.FILE_NAME_CONFIG,
+                        ""
+                    )
+        }
+
+        bottomSheetView.findViewById<MaterialButton>(R.id.cheats).setOnClickListener {
+            val action = CheatsFragmentDirections.actionGlobalCheatsFragment(holder.game.titleId)
+            view.findNavController().navigate(action)
+            bottomSheetDialog.dismiss()
+        }
+        
+        bottomSheetDialog.show()
     }
 
     private fun isValidGame(extension: String): Boolean {
