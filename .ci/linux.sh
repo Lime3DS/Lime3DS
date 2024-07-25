914dc13f6718bd57a55f171971d6687a612627cf
Miguel <81558772+Miguel-hrvs@users.noreply.github.com>
2024-07-25 13:42:19 +0200

    bundle qt wayland into appimage by rtiangha (#212)

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -2,6 +2,9 @@
 
 if [ "$TARGET" = "appimage" ]; then
     export EXTRA_CMAKE_FLAGS=(-DCMAKE_LINKER=/etc/bin/ld.lld)
+    # Bundle required QT wayland libraries
+    export EXTRA_QT_PLUGINS="waylandcompositor"
+    export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so"
 else
     # For the linux-fresh verification target, verify compilation without PCH as well.
     export EXTRA_CMAKE_FLAGS=(-DCITRA_USE_PRECOMPILED_HEADERS=OFF)
