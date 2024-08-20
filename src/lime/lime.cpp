43e5c649d30fa30ea39bd9da0e2402bcf4302203
OpenSauce04 <opensauce04@gmail.com>
2024-08-20 19:12:28 +0100

    Reverted unwarranted license header changes

--- src/lime/lime.cpp
+++ src/lime/lime.cpp
@@ -1,4 +1,4 @@
-// Copyright Citra Emulator Project / Lime3DS Emulator Project
+// Copyright 2014 Citra Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
