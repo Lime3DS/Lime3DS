65c4c6ec4ea77f580170802da537382b9edf0e6f
OpenSauce04 <opensauce04@gmail.com>
2024-09-23 18:41:06 +0100

    android: Updated AGP to 8.6.1

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -1,11 +1,11 @@
-// Copyright 2023 Citra Emulator Project
+// Copyright Citra Emulator Project / Lime3DS Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
 }
