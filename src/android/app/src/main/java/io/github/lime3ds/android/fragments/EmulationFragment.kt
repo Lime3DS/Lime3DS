f92b9b463c0c0166b861ff33deab31be85d1065c
kleidis <167202775+kleidis@users.noreply.github.com>
2024-09-25 18:34:56 +0200

    android: Allow saving to quickslot from UI
    Idk why this was done by the original author as there are no conflicts

--- src/android/app/src/main/java/io/github/lime3ds/android/fragments/EmulationFragment.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/fragments/EmulationFragment.kt
@@ -534,7 +534,6 @@ class EmulationFragment : Fragment(), SurfaceHolder.Callback, Choreographer.Fram
                 val slot = i
                 var enableClick = isSaving
                 val text = if (slot == NativeLibrary.QUICKSAVE_SLOT) {
-                    enableClick = false
                     getString(R.string.emulation_quicksave_slot)
                 } else {
                     getString(R.string.emulation_empty_state_slot, slot)
@@ -558,8 +557,6 @@ class EmulationFragment : Fragment(), SurfaceHolder.Callback, Choreographer.Fram
         savestates?.forEach {
             var enableClick = true
             val text = if(it.slot == NativeLibrary.QUICKSAVE_SLOT) {
-                // do not allow saving in quicksave slot
-                enableClick = !isSaving
                 getString(R.string.emulation_occupied_quicksave_slot, it.time)
             } else{
                 getString(R.string.emulation_occupied_state_slot, it.slot, it.time)
