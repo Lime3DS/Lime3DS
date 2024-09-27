af7ccc19554d38e6a5e21966baf40656fe7797bd
David Griswold <novachild@gmail.com>
2024-09-27 12:02:19 +0100

    Add header descriptions for dpad axis and dpad button bindings
    Co-authored-by: OpenSauce04 <opensauce04@gmail.com>

--- src/android/app/src/main/java/io/github/lime3ds/android/features/settings/ui/SettingsFragmentPresenter.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/features/settings/ui/SettingsFragmentPresenter.kt
@@ -650,14 +650,12 @@ class SettingsFragmentPresenter(private val fragmentView: SettingsFragmentView) 
                 add(InputBindingSetting(button, Settings.axisTitles[i]))
             }
 
-            // TODO: Improve the integration of the two dpad types to be
-            //       less user-facingly hacky
-            add(HeaderSetting(R.string.controller_dpad_axis))
+            add(HeaderSetting(R.string.controller_dpad_axis,R.string.controller_dpad_axis_description))
             Settings.dPadAxisKeys.forEachIndexed { i: Int, key: String ->
                 val button = getInputObject(key)
                 add(InputBindingSetting(button, Settings.axisTitles[i]))
             }
-            add(HeaderSetting(R.string.controller_dpad_button))
+            add(HeaderSetting(R.string.controller_dpad_button,R.string.controller_dpad_button_description))
             Settings.dPadButtonKeys.forEachIndexed { i: Int, key: String ->
                 val button = getInputObject(key)
                 add(InputBindingSetting(button, Settings.dpadTitles[i]))
