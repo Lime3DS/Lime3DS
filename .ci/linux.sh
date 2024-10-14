0a5536aebe760311da114cbd7261c91c2578f224
OpenSauce04 <opensauce04@gmail.com>
2024-10-14 20:34:29 +0100

    Revert "bundle qt wayland into appimage by rtiangha (#212)"
    This reverts commit 8fd0fb8bf01c8dcfa2c334b59a29d370921eac2d.
    
    See #496 for more details

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -2,9 +2,6 @@
 
 if [ "$TARGET" = "appimage" ]; then
     export EXTRA_CMAKE_FLAGS=(-DCMAKE_LINKER=/etc/bin/ld.lld)
-    # Bundle required QT wayland libraries
-    export EXTRA_QT_PLUGINS="waylandcompositor"
-    export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so"
 else
     # For the linux-fresh verification target, verify compilation without PCH as well.
     export EXTRA_CMAKE_FLAGS=(-DLIME3DS_USE_PRECOMPILED_HEADERS=OFF)
